485e56fc1ce1ac7acd437d9ea978acc4cd762679
jiangjiawei1103 <waynechuang97@gmail.com>
2022-10-20 18:52:09 +0800

    feat: Do quick EDA

---
+++ metadata.py
@@ -0,0 +1,12 @@
+"""
+Project-specific metadata for global access.
+Author: JiaWei Jiang
+"""
+# =Project A=
+# =Project B=
+B = {
+    "PK": ["time", "layer"],  # Primary key
+    "FEAT": ["b", "c", "d", "e", "f", "g", "h", "i"],  # Feature columns
+    "TARGET": "MaxWear",  # Target column
+    "PROC_LAYERS": 46,  # Number of processing layers
+}
