3805a6cf7599c66d33d2f3d91dd5524128302537
jiangjiawei1103 <waynechuang97@gmail.com>
2022-10-21 22:05:04 +0800

    feat: Do more EDA and a quick modeling w/ RF

--- metadata.py
+++ metadata.py
@@ -6,7 +6,8 @@ Author: JiaWei Jiang
 # =Project B=
 B = {
     "PK": ["time", "layer"],  # Primary key
-    "FEAT": ["b", "c", "d", "e", "f", "g", "h", "i"],  # Feature columns
+    "SG_FEAT": ["b", "c", "d", "e", "f", "g", "h", "i"],  # Feature columns of sg
+    "SPIKE_FEAT": ["b", "c", "d"],  # Feature columns of spike
     "TARGET": "MaxWear",  # Target column
     "PROC_LAYERS": 46,  # Number of processing layers
 }
