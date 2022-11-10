afd8863c0f2e8e074f49f5265d7b2501516f035f
jiangjiawei1103 <waynechuang97@gmail.com>
2022-11-11 00:35:16 +0800

    feat: Add data augmentation test (incomplete)

--- metadata.py
+++ metadata.py
@@ -4,12 +4,10 @@ Author: JiaWei Jiang
 """
 # =Project A=
 # =Project B=
-B = {
-    "PK": ["time", "layer"],  # Primary key
-    "SG_FEAT": ["b", "c", "d", "e", "f", "g", "h", "i"],  # Feature columns of sg
-    "SPIKE_FEAT": ["b", "c", "d"],  # Feature columns of spike
-    "TARGET": "MaxWear",  # Target column
-    "PROC_LAYERS": 46,  # Number of processing layers
-    "GP1_LEN": 26,  # Number of processing layers in group1
-    "GP2_LEN": 20,  # Number of processing layers in group2
-}
+PK = ["time", "layer"]  # Primary key
+SG_FEAT = ["b", "c", "d", "e", "f", "g", "h", "i"]  # Feature columns of sg
+SPIKE_FEAT = ["b", "c", "d"]  # Feature columns of spike
+TARGET = "MaxWear"  # Target column
+PROC_LAYERS = 46  # Number of processing layers
+GP1_LEN = 26  # Number of processing layers in group1
+GP2_LEN = 20  # Number of processing layers in group2
